669a7fead9a014ea20300d252bd5c4247f540dbb
oboy21 <oboy11b@gmail.com>
2024-05-14 17:15:27 +0300

    stored extracted data in text files

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import selectorlib
+
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+def send_email():
+    print("Email was sent!")
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+    store(extracted)
+    if extracted != "No Upcoming Tours":
+        if not extracted in "data.txt":
+              store(extracted)
+              send_email()
