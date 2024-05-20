5525a50385d13faf66637e6801e8e4e0e2be50c8
oboy21 <oboy11b@gmail.com>
2024-05-20 17:13:57 +0300

    student project

--- main.py
+++ main.py
@@ -1,70 +1,38 @@
-import time
-
 import requests
 import selectorlib
-import smtplib, ssl
-import os
 import sqlite3
 
-"INSERT INTO events VALUES ('Tigers', 'Tiger City', '2088.10.14')"
-"SELECT * FROM events WHERE date='2088.10.15' "
-URL = "https://programmer100.pythonanywhere.com/tours/"
+from datetime import datetime
 
 connection = sqlite3.connect("data.db")
-cursor = connection.cursor()
+
+URL = "http://programmer100.pythonanywhere.com/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
 def scrape(url):
     """Scrape the page source from the URL"""
-    response = requests.get(url)
+    response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
 
 
 def extract(source):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
 
-
-def send_email():
-    def send_email(message):
-        host = "smtp.gmail.com"
-        port = 465
-
-        username = ""
-        password = "here_goes_your_gmail_password"
-
-        receiver = ""
-        context = ssl.create_default_context()
-
-        with smtplib.SMTP_SSL(host, port, context=context) as server:
-            server.login(username, password)
-            server.sendmail(username, receiver, message)
-
-    print("Email was sent!")
 
 def store(extracted):
-   row = extracted.split(",")
-   row = [item.strip() for item in row]
-   cursor = connection.cursor()
-   cursor.execute("INSERT INTO events VALUES(?,?,*)", row)
-   connection.commit()
-def read(extracted):
-   row = extracted.split(",")
-   row =[item.strip() for item in row]
-   band, city, date = row
-   cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-   rows = cursor.fetchall()
-   print(rows)
-   return rows
+    now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO temperatures VALUES(?, ?)", (now, extracted))
+    connection.commit()
+
 
 if __name__ == "__main__":
-    while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
-        print(extracted)
-
-        if extracted != "No Upcoming Tours":
-            row = read(extracted)
-            if extracted not in "data.txt":
-                  store(extracted)
-                  send_email(message="Hey, new event was found!")
-        time.sleep(2)
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+    store(extracted)
